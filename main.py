94eb1eb7460b61e51978672f6c10abc6e02c3b3f
Nikan <52320631+nikanmog@users.noreply.github.com>
2022-02-04 15:42:55 +0100

    Added multi-threading

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import datetime
 import time
+
+import grequests
 import requests
 from bs4 import BeautifulSoup
 
 LIBRARY = "Main Campus"
 HEADER = {"Cookie": "GET FROM WEBSITE BY INSPECTING A POST REQUEST"}
-captcha_1 = {
-    "captcha_sid": 1234,
-    "captcha_token": "1234",
-    "captcha_response": "1234"
-}
-captcha_2 = {
-    "captcha_sid": 1234,
-    "captcha_token": "1234",
-    "captcha_response": "1234"
-}
+captcha = [
+    {
+        "captcha_sid": 1,
+        "captcha_token": "1",
+        "captcha_response": "1"
+    },
+    {
+        "captcha_sid": 1,
+        "captcha_token": "1",
+        "captcha_response": "1"
+    }
+]
+
 FORM_DATA = {
     "form_build_id": "GET FROM WEBSITE BY INSPECTING A POST REQUEST",
     "form_id": "registration_form",
@@ -26,34 +31,39 @@ FORM_DATA = {
     "field_benutzungsrichtlinien[und]": 1,
     "field_datenschutzerklaerung[und]": 1,
     "field_url_token[und][0][value]": None,
-    "op": "Anmelden",
+    "op": "Save Registration",
     "homepage": None
 }
 BASE_URL = "https://www.ub.tum.de"
 
 
 def book_seat() -> bool:
-    is_seat_booked = False
     raw_page = requests.get(BASE_URL + "/en/reserve-study-desks")
     bs_page = BeautifulSoup(raw_page.content, "html.parser")
 
     libraries = bs_page.find_all("td", class_="views-field views-field-field-teilbibliothek")
     reservation_links = bs_page.find_all("td", class_="views-field views-field-views-conditional internlink")
     lib_indexes = [index for index, library in enumerate(libraries) if library.text.strip() == LIBRARY]
-    captcha_switch = 0
+    async_list = []
     for index in lib_indexes:
         if reservation_links[index].text.strip() != "ausgebucht":
             reservation_id = reservation_links[index].find("a")['href'].split("/")[-1]
-            url = BASE_URL + "/reserve/" + reservation_id
-            print(f"Requesting: {url} at: {datetime.datetime.now()}")
-            FORM_DATA.update(captcha_1 if captcha_switch % 2 == 0 else captcha_2)
-            captcha_switch += 1
-            response = requests.request("POST", url=url, headers=HEADER, data=FORM_DATA)
-            print(response.headers)
-            is_seat_booked = True
+            url = BASE_URL + "/en/reserve/" + reservation_id
+            FORM_DATA.update(captcha[len(async_list)])
+            async_list.append(grequests.post(url=url, headers=HEADER, data=FORM_DATA))
         else:
             print(f"There are no more seats for: {libraries[index].text.strip()} at: {datetime.datetime.now()}")
-    return is_seat_booked
+
+    successful_bookings = 0
+    while successful_bookings < len(async_list):
+        for resp in grequests.imap(async_list, len(async_list)):
+            link = resp.headers.get("Link")
+            print(f"Requesting {link}")
+            if "confirmation" in link:
+                print(f"Success {successful_bookings}")
+                successful_bookings += 1
+
+    return successful_bookings == len(async_list)
 
 
 def time_until_execution():
@@ -63,9 +73,12 @@ def time_until_execution():
 
 
 print(f"Will run in {time_until_execution()}")
-time.sleep(time_until_execution() - 1)
-for i in range(10):
-    time.sleep(2)
+time.sleep(time_until_execution())
+for i in range(3600):
     print("Sending Request")
-    if book_seat():
-        break
+    try:
+        if book_seat():
+            break
+    except Exception as e:
+        print(e)
+    time.sleep(1)
